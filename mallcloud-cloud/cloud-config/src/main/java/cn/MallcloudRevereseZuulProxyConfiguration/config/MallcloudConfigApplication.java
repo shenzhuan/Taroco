62fb155ce8ddbc25b11f993d3e347bac7dd046e4
1439226817@qq.com <1439226817@qq.com>
2019-02-21 00:50:41 +0800

    sql

--- mallcloud-cloud/cloud-config/src/main/java/cn/MallcloudRevereseZuulProxyConfiguration/config/MallcloudConfigApplication.java
+++ mallcloud-cloud/cloud-config/src/main/java/cn/MallcloudRevereseZuulProxyConfiguration/config/MallcloudConfigApplication.java
@@ -1,4 +1,4 @@
-package cn.mallcloud.config;
+package cn.MallcloudRevereseZuulProxyConfiguration.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
